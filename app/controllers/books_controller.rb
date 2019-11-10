f8e0684f9bfe324e430d76119cb9dc413c4e2b0b
tabatalisboa <tabatalisboa@pm.me>
2019-11-10 19:30:18 -0300

    Add simple_form gem & Create BooksController Actions + Views + Routes

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,48 @@
+class BooksController < ApplicationController
+  # before_action
+
+  def index
+    @books = Book.all.order("created_at DESC")
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to root_path
+    else
+      :new
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      :edit
+    end
+  end
+
+  def destroy
+
+  end
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :description, :author)
+  end
+end
